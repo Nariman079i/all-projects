dbaf813b5ed95fa8edc88bce584f7e15a321fe26
Nariman <Nariman079i>
2023-05-24 02:42:39 +0300

    d

--- BARKHAN-CRM/backend/offermvp/urls.py
+++ BARKHAN-CRM/backend/offermvp/urls.py
@@ -9,7 +9,7 @@ from offermvp import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('barkan/', include('barkan.urls'))]
+    path('barkan/', include('barkan.api.urls'))]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
